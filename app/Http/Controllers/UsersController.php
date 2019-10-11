60a21647e5022848466a730c42ed124c6233a4bd
pooh <925005684@qq.com>
2019-10-11 12:30:19 +0000

    Add user following

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -15,6 +15,9 @@ class UsersController extends Controller
         $this->middleware('auth', [
             'except' => ['show', 'create', 'store', 'index', 'confirmEmail']
         ]);
+        $this->middleware('auth', [
+            'except' => ['show', 'create', 'store']
+        ]);
         $this->middleware('guest', [
             'only' => ['create']
         ]);
@@ -116,6 +119,20 @@ class UsersController extends Controller
         Auth::login($user);
         session()->flash('success', '恭喜你，激活成功！');
         return redirect()->route('users.show', [$user]);
+    }
+
+    public function followings(User $user)
+    {
+        $users = $user->followings()->paginate(30);
+        $title = '关注的人';
+        return view('users.show_follow', compact('users', 'title'));
+    }
+
+    public function followers(User $user)
+    {
+        $users = $user->followers()->paginate(30);
+        $title = '粉丝';
+        return view('users.show_follow', compact('users', 'title'));
     }
 
 }
